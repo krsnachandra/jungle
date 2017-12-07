ce75b27020857c4087fbe866732920bf7916f479
Chandra <krsna.c@gmail.com>
2017-12-07 23:23:46 +0000

    changed 'put' to 'post' to better conform to html spec

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    put    :add_item
+    post    :add_item
     delete :remove_item
   end
 
