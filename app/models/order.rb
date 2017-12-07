bed09c5cbe7e9f3bd3f0248c11835cb3eaf998d7
Chandra <krsna.c@gmail.com>
2017-12-07 23:25:26 +0000

    added a total price method

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,12 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  def total_price
+    answer = 0
+    line_items.each do |line_item|
+      answer += line_item.total_price
+    end
+    answer
+  end
+
 end
