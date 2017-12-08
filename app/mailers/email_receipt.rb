6e42aa59510787af65ff1818a5f37a4612c988c0
Chandra <krsna.c@gmail.com>
2017-12-08 19:24:07 +0000

    fixed the url to link correctly to the order page

--- app/mailers/email_receipt.rb
+++ app/mailers/email_receipt.rb
@@ -5,7 +5,7 @@ class EmailReceipt < ApplicationMailer
   def email_receipt(order)
     @order = order
     # @url  = "/order/#{order.id}"
-    @url = "localhost:3000/order/#{order.id}"
+    @url = "localhost:3000/orders/#{order.id}"
     mail(to: 'huxof@c1oramn.com', subject: "Thank you for your order (Order # )")
   end
 
