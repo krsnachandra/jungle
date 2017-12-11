5191746892bd77bfdd0d048c6028f475984bac85
Chandra <krsna.c@gmail.com>
2017-12-11 23:59:54 +0000

    added test suite for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-  validates :email, uniqueness: true
+
+  validates :email, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   has_many :reviews
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    user && user.authenticate(password)
+  end
 
 end
